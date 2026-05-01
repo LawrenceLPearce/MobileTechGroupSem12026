9789931c895fcedbe4e8dba3aa01e23e320711f5
Tushar <u3285413@uni.canberra.edu.au>
2026-05-01 23:45:36 +1000

    made layout for act 6 and act 7 and made buttons work.

--- app/src/main/java/com/example/mtgroupprojectsem1/Activity6.java
+++ app/src/main/java/com/example/mtgroupprojectsem1/Activity6.java
@@ -1,6 +1,11 @@
 package com.example.mtgroupprojectsem1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+
 public class Activity6 extends AppCompatActivity {
+
+    ListView listView;
+    Button buttonAdd;
+
+    ArrayList<String> itemList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_6);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listView = findViewById(R.id.listViewAct6);
+        buttonAdd = findViewById(R.id.buttonAdd);
+
+        // TEMP DATA
+        itemList = new ArrayList<>();
+        itemList.add("Sample 1");
+        itemList.add("Sample 2");
+        itemList.add("Sample 3");
+
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                itemList
+        );
+
+        listView.setAdapter(adapter);
+
+        // list item click
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent intent = new Intent(Activity6.this, Activity7.class);
+            startActivity(intent);
+
+        });
+
+        // button
+        buttonAdd.setOnClickListener(v -> {
+            Intent intent = new Intent(Activity6.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 }
+
